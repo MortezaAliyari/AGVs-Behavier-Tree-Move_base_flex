5831f393aaf2ccb56912d7bb7ec180f91c1bf332
Morteza Aliyari <79801785+MortezaAliyari@users.noreply.github.com>
2022-08-18 13:15:27 +0800

    Add files via upload
    rtmode_server and rt_mode client added to actionlib package.

---
+++ actionlib_tutorials/src/rtmode_client.cpp
@@ -0,0 +1,45 @@
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include <actionlib_tutorials/RobotmodeAction.h>
+
+int main (int argc, char **argv)
+{
+  ros::init(argc, argv, "rtmode_client");
+
+  // create the action client
+  // true causes the client to spin its own thread
+  actionlib::SimpleActionClient<actionlib_tutorials::RobotmodeAction> ac("Robotmode", true);
+
+  ROS_INFO("Waiting for rtmode action server to start.");
+  // wait for the action server to start
+  ac.waitForServer(); //will wait for infinite time
+
+  ROS_INFO("Action server started, sending Robot Mode.");
+  // send a goal to the action
+  std::vector<float> single{0.5,-1.0, -1};
+  actionlib_tutorials::RobotmodeGoal goal;
+  goal.order = 20;
+  goal.gm=0;
+  goal.nav=1;
+  goal.nav_single=1;
+  goal.nav_file=0;
+  goal.singledata={0.5,-1.0, -1};
+  goal.singlename="delay";
+  goal.filename="A1";
+  ac.sendGoal(goal);
+
+  //wait for the action to return
+  bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
+
+  if (finished_before_timeout)
+  {
+    actionlib::SimpleClientGoalState state = ac.getState();
+    ROS_INFO("Action finished: %s",state.toString().c_str());
+  }
+  else
+    ROS_INFO("Action did not finish before the time out.");
+
+  //exit
+  return 0;
+}
---
+++ actionlib_tutorials/src/rtmode_server.cpp
@@ -0,0 +1,138 @@
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <actionlib_tutorials/RobotmodeAction.h>
+#include "std_msgs/String.h"
+#include </home/morteza/catkin_ws/src/mbf_tutorials/mbf_advanced/include/mbf_advanced/mbf_rtmode_client.h>
+
+using namespace std;
+class RobotmodeAction
+{
+protected:
+
+  ros::NodeHandle nh_;
+  std_msgs::String Nav_CM_msg;
+  ros::Publisher Nav_CM_pub = nh_.advertise<std_msgs::String>("Nav_CM", 1);
+  actionlib::SimpleActionServer<actionlib_tutorials::RobotmodeAction> as_; // NodeHandle instance must be created before this line. Otherwise strange error occurs.
+  std::string action_name_;
+  // create messages that are used to published feedback/result
+  actionlib_tutorials::RobotmodeFeedback feedback_;
+  actionlib_tutorials::RobotmodeResult result_;
+
+public:
+  mbf_advanced::MBFCircleClient mbfclient;
+  RobotmodeAction(std::string name) :
+    as_(nh_, name, boost::bind(&RobotmodeAction::executeCB, this, _1), false),
+    action_name_(name)
+  {
+    Nav_CM_msg.data="DE";
+    as_.start();
+    ros::Duration(0.01).sleep();//without this delay ros doesn't publish Nav_CM_msg
+    Nav_CM_pub.publish(Nav_CM_msg);
+    cout<<"///////////////////////////Change the navigation mode///////////////////////////"<<endl;
+  }
+
+  ~RobotmodeAction(void)
+  {
+  }
+
+  void executeCB(const actionlib_tutorials::RobotmodeGoalConstPtr &goal)
+  {
+    // helper variables
+    ros::Rate r(1);
+    bool success = true;
+
+    // push_back the seeds for the fibonacci sequence
+    feedback_.sequence.clear();
+    feedback_.sequence.push_back(0);
+    feedback_.sequence.push_back(1);
+
+    // publish info to the console for the user
+    //ROS_INFO("%s: Executing, creating robotmode sequence of order %i with seeds %i, %i", action_name_.c_str(), goal->order, feedback_.sequence[0], feedback_.sequence[1]);
+    ROS_INFO("Action messge is as below: ");
+    ROS_INFO("Gmapping value is %d",goal->gm);
+    ROS_INFO("Navigation value is %d",goal->nav);
+
+    ROS_INFO("Navigation mode is single %d",goal->nav_single);
+
+    ROS_INFO("Navigation mode is file   %d",goal->nav_file);
+
+    ROS_INFO("Single command value is %f",goal->singledata[1]);
+    ROS_INFO("Single task name is %s",goal->singlename.c_str());
+    ROS_INFO("navigation file name is %s",goal->filename.c_str());
+    if (goal->nav==0 && goal->gm==1){
+      Nav_CM_msg.data="ED";
+      Nav_CM_pub.publish(Nav_CM_msg);
+      cout<<"///////////////////////////Gmapping is Enabled now control the robot! "<<endl;
+
+    }
+    else if(goal->nav==1 && goal->gm==0){
+      Nav_CM_msg.data="DE";
+      Nav_CM_pub.publish(Nav_CM_msg);
+      cout<<"///////////////////////////Navigation is Enabled now select navigation mode! "<<endl;
+      if(goal->nav_single==1 && goal->nav_file==0){
+
+        cout<<"///////////////////////////Single command is selected!"<<endl;
+        float x=goal->singledata[0];
+        float y=goal->singledata[1];
+        mbf_advanced::Quaternion q;
+        q=mbf_advanced::ToQuaternion(goal->singledata[2],0,0);
+        mbfclient.move_to_goal(mbfclient.create_goal(x,y,0,q.x,q.y,q.z,q.w));
+
+      }
+      else if (goal->nav_single==0 && goal->nav_file==1) {
+
+        cout<<"///////////////////////////File command is selected!"<<endl;
+      }
+      else {
+        cout<<"///////////////////////////Enable one of the modes: "<<
+            "signle  or file Navigation\n"<<
+            "///////////////////////////The defualt is single"<<endl;
+      }
+
+    }
+    else
+      cout<<"///////////////////////////Enable one of the modes: "<<
+          "Gmapping or Navigation\n"<<
+          "///////////////////////////The defualt is Navigation"<<endl;
+
+    // start executing the action
+    for(int i=1; i<=goal->order; i++)
+    {
+      // check that preempt has not been requested by the client
+      if (as_.isPreemptRequested() || !ros::ok())
+      {
+        ROS_INFO("%s: Preempted", action_name_.c_str());
+        // set the action state to preempted
+        as_.setPreempted();
+        success = false;
+        break;
+      }
+      feedback_.sequence.push_back(feedback_.sequence[i] + feedback_.sequence[i-1]);
+      // publish the feedback
+      as_.publishFeedback(feedback_);
+      // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
+      r.sleep();
+    }
+
+    if(success)
+    {
+      result_.sequence = feedback_.sequence;
+      ROS_INFO("%s: Succeeded", action_name_.c_str());
+      // set the action state to succeeded
+      as_.setSucceeded(result_);
+    }
+  }
+
+
+};
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "rtmode_server");
+
+  RobotmodeAction Robotmode("Robotmode");
+  ros::spin();
+
+  return 0;
+}
