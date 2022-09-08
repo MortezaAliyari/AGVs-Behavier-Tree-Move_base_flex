d0c961f386103311aa35c86f0059832a18fed9cc
Morteza Aliyari <79801785+MortezaAliyari@users.noreply.github.com>
2022-09-08 15:33:16 +0800

    Update rtmode_server.cpp
    There was a problem with two action clients it didn't wait for the result. so I just publish results from nav file node rtmode_server and then send feedback or result by this server to the client.

--- actionlib_tutorials/src/rtmode_server.cpp
+++ actionlib_tutorials/src/rtmode_server.cpp
@@ -3,6 +3,7 @@
 #include <actionlib_tutorials/RobotmodeAction.h>
 #include "std_msgs/String.h"
 #include </home/morteza/catkin_ws/src/mbf_tutorials/mbf_advanced/include/mbf_advanced/mbf_rtmode_client.h>
+#include </home/morteza/catkin_ws/src/mbf_tutorials/mbf_advanced/include/mbf_advanced/mbf_circle_client2.h>
 
 using namespace std;
 class RobotmodeAction
@@ -16,23 +17,71 @@ protected:
   ros::Publisher Nav_CM_pub      = nh_.advertise<std_msgs::String>("Nav_CM", 1);
   ros::Publisher Navfilename_pub = nh_.advertise<std_msgs::String>("Navfilename", 1);
 
-  actionlib::SimpleActionServer<actionlib_tutorials::RobotmodeAction> as_; // NodeHandle instance must be created before this line. Otherwise strange error occurs.
+
+  actionlib::SimpleActionServer<actionlib_tutorials::RobotmodeAction> asrtmode_; // NodeHandle instance must be created before this line. Otherwise strange error occurs.
+//  actionlib::SimpleActionClient<mbf_msgs::MoveBaseAction> acmbfnavfile_;
+
   std::string action_name_;
   // create messages that are used to published feedback/result
   actionlib_tutorials::RobotmodeFeedback feedback_;
   actionlib_tutorials::RobotmodeResult result_;
 
 public:
-  mbf_rtmode::MBFCircleClient mbfclient;
+  mbf_rtmode::MBFCircleClient mbfclient_Navsingle; //relate to receiving feedback when running single commands
+  //mbf_advanced::MBFCircleClient mbfclient_Navfile; //relate to receiving feedback when running scenario or Navigation file
+  bool nfr=false,fbnfr=false,sfbnfr=false,ffbnfr=false;
+  ros::NodeHandle n;
+  ros::Subscriber NavfileResults_sub=n.subscribe("NavfileResults", 10,&RobotmodeAction::NavfileResults_Callback,this); //new format of subscribing topic in constructer function of a class;
+
+  string NavfileResults;
+
+  void NavfileResults_Callback(const std_msgs::StringConstPtr& msg)
+  {
+    size_t found1 = msg->data.find("successfuly reached to target number");
+    size_t found2 = msg->data.find("successfuly finished scenario!");
+    size_t found3 = msg->data.find("Failed to reach to target number");
+    size_t found4 = msg->data.find("scenario Failed!");
+    nfr=true;
+    if (found1 != string::npos)
+    {
+      fbnfr=true;//target feedback of navfile result
+      NavfileResults=msg->data;
+      ROS_INFO("found1");
+    }
+    else if (found2 != string::npos) {
+      sfbnfr=true;//scenario feedback of navfile result
+      NavfileResults=msg->data;
+      ROS_INFO("found2");
+    }
+    else if (found3 != string::npos) {
+      fbnfr=true;//target feedback of navfile result
+      NavfileResults=msg->data;
+      ROS_INFO("found3");
+    }
+    else if (found4 != string::npos) {
+      ffbnfr=true;//scenario feedback of navfile result
+      NavfileResults=msg->data;
+      ROS_INFO("found4");
+    }
+    else {
+      fbnfr =false;//target   feedback of navfile result
+      sfbnfr=false;//scenario feedback of navfile result
+      ffbnfr=false;
+      ROS_INFO("NavfileResults=msg->data nothing matched");
+    }
+  }
+
+  /////////////////////////
   RobotmodeAction(std::string name) :
-    as_(nh_, name, boost::bind(&RobotmodeAction::executeCB, this, _1), false),
+    asrtmode_(nh_, name, boost::bind(&RobotmodeAction::executeCB, this, _1), false),
     action_name_(name)
   {
     Nav_CM_msg.data="DE";
-    as_.start();
+
+    asrtmode_.start();
     ros::Duration(0.01).sleep();//without this delay ros doesn't publish Nav_CM_msg
     Nav_CM_pub.publish(Nav_CM_msg);
-    cout<<"////////////////////////////////////////////Change the navigation mode///////////////////////////"<<endl;
+    ROS_INFO("Robot is waiting for Client request!");
   }
 
   ~RobotmodeAction(void)
@@ -45,72 +94,74 @@ public:
     ros::Rate r(1);
     bool success = true;
 
-    // push_back the seeds for the fibonacci sequence
-    feedback_.sequence.clear();
-    feedback_.sequence.push_back(0);
+
+    feedback_.targetpoint.clear();
+    feedback_.scenario.clear();
+
     int n=0;
 
     // publish info to the console for the user
-    //ROS_INFO("%s: Executing, creating robotmode sequence of order %i with seeds %i, %i", action_name_.c_str(), goal->order, feedback_.sequence[0], feedback_.sequence[1]);
     ROS_INFO("Action messge is as below: ");
-    ROS_INFO("Gmapping value is %d",goal->gm);
-    ROS_INFO("Navigation value is %d",goal->nav);
+    ROS_INFO("                          Gmapping value is %d",goal->gm);
+    ROS_INFO("                          Navigation value is %d",goal->nav);
 
-    ROS_INFO("Navigation mode is single %d",goal->nav_single);
+    ROS_INFO("                          Single Navigation value is %d",goal->nav_single);
 
-    ROS_INFO("Navigation mode is file   %d",goal->nav_file);
+    ROS_INFO("                          File   Navigation value is %d",goal->nav_file);
 
-    ROS_INFO("Single command value is %f",goal->singledata[1]);
-    ROS_INFO("Single task name is %s",goal->singlename.c_str());
-    ROS_INFO("navigation file name is %s",goal->filename.c_str());
+    ROS_INFO("                          Single command value is %f",goal->singledata[1]);
+    ROS_INFO("                          Single task name is %s",goal->singlename.c_str());
+    ROS_INFO("                          navigation file name is %s",goal->filename.c_str());
     if (goal->nav==0 && goal->gm==1){
       Nav_CM_msg.data="ED";
       Nav_CM_pub.publish(Nav_CM_msg);
-      cout<<"////////////////////////////////////////////Gmapping is Enabled now control the robot! "<<endl;
-
+      ROS_INFO("Gmapping is Enabled use keyboard to control the robot!");
     }
-    else if(goal->nav==1 && goal->gm==0){
+    else if(goal->nav==1 && goal->gm==0)
+    {
       Nav_CM_msg.data="DE";
       Nav_CM_pub.publish(Nav_CM_msg);
-      cout<<"////////////////////////////////////////////Navigation is Enabled now select navigation mode! "<<endl;
+      ROS_INFO("Navigation is Enabled");
       if(goal->nav_single==1 && goal->nav_file==0){
-
-        cout<<"////////////////////////////////////////////Single command is selected!"<<endl;
-        //int arrSize = sizeof(goal->singledata)/sizeof(goal->singledata[0]);
+        ROS_INFO("Single command is selected");
         int arrSize=goal->order;
-        cout<<"////////////////////////////////////////////array size [%d] "<<arrSize<<endl;
-
+        ROS_INFO("Number of single commands is : %d",arrSize);
         for (int i=0;i<arrSize;i=i+3) {
-          cout<<M_PI<<" =? "<<goal->singledata[1+i]<<endl;
+         // cout<<M_PI<<" =? "<<goal->singledata[1+i]<<endl;
 
           if (goal->singledata[1+i]+goal->singledata[2+i]==M_PI+M_PI){
-            cout<<"////////////////////////////////////////////Robot sleeped for [%f] second"<<goal->singledata[i]<< ", i : "<<i<<endl;
-
+            ROS_INFO("Robot sleeped for [%f] second",goal->singledata[i]);
             ros::Duration(goal->singledata[i]).sleep();
           }
           else {
-            cout<<"////////////////////////////////////////////Moving to goal point! "<<goal->singledata[i]<<" , "<<goal->singledata[i+1]<<" ,i: "<<i<<endl;
+            ROS_INFO("Robot Moving to goal point! x= %f , y=%f",goal->singledata[i],goal->singledata[i+1]);
             float x=goal->singledata[i];
             float y=goal->singledata[i+1];
             mbf_rtmode::Quaternion q;
             q=mbf_rtmode::ToQuaternion(goal->singledata[i+2],0,0);
-            auto result=mbfclient.move_to_goal(mbfclient.create_goal(x,y,0,q.x,q.y,q.z,q.w));
+            auto result=mbfclient_Navsingle.move_to_goal(mbfclient_Navsingle.create_goal(x,y,0,q.x,q.y,q.z,q.w));
 
             if(result->outcome ==mbf_msgs::MoveBaseResult::SUCCESS){
               n++;
-              feedback_.sequence.push_back(n);
+              feedback_.targetpoint.append("Success on target number ");
+              feedback_.targetpoint.append(to_string(n));
               // publish the feedback
-              as_.publishFeedback(feedback_);
+              asrtmode_.publishFeedback(feedback_);
               // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
               r.sleep();
-              cout<<"////////////////////////////////////////////reached to the one of the target points"<<endl;
-
+              ROS_INFO("Robot reached to the one of the target points");
             }
-            if (as_.isPreemptRequested() || !ros::ok())
+            else {
+              success = false;
+            }
+            if (asrtmode_.isPreemptRequested() || !ros::ok())
             {
-              ROS_INFO("%s: Preempted", action_name_.c_str());
+              ROS_INFO("%s  server: Preempted or cancelled", action_name_.c_str());
+              ROS_INFO("MBF server: Preempted or cancelled");
+
               // set the action state to preempted
-              as_.setPreempted();
+              asrtmode_.setPreempted();
+              mbfclient_Navsingle.acmbf_.cancelAllGoals();
               success = false;
               break;
             }
@@ -118,31 +169,71 @@ public:
           }
         }
       }
-      else if (goal->nav_single==0 && goal->nav_file==1) {
+      else if (goal->nav_single==0 && goal->nav_file==1)
+      {
         Navfilename_msg.data=goal->filename;
         Navfilename_pub.publish(Navfilename_msg);
-        cout<<"////////////////////////////////////////////["<<goal->filename.c_str()<<"] File is selected to navigate!"<<endl;
+        while(1)
+        {
+          ROS_INFO("waiting for result of File navigation mode!");
+          while(!nfr); //stup untile receive result from MBF action
+          nfr=false;
+          if(sfbnfr)
+          {// send successful result to rtmode client
+
+            sfbnfr=false;
+            success = true;
+            ROS_INFO("sfbnfr");
+            break;
+          }
+          else if (ffbnfr)
+          {//send fail result to rtmode client
+            ffbnfr=false;
+            success = false;
+            ROS_INFO("ffbnfr");
+            break;
+          }
+          else if (fbnfr)
+          {//send feedback to rtmode client
+            feedback_.scenario.append(NavfileResults);
+            asrtmode_.publishFeedback(feedback_);
+            fbnfr=false;
+            ROS_INFO("fbnfr");
+          }
+          else {
+            ROS_INFO("No result!");
+            sfbnfr=false;
+            fbnfr=false;
+
+          }
+        }
+
       }
       else {
-        cout<<"////////////////////////////////////////////Enable one of the modes: "<<
-            "signle  or file Navigation\n"<<
-            "////////////////////////////////////////////The defualt is single"<<endl;
+          ROS_INFO("Enable one of the modes:");
+          ROS_INFO("                        signle  or file Navigation");
+          ROS_INFO("                        The defualt is single");
       }
 
     }
-    else
-      cout<<"////////////////////////////////////////////Enable one of the modes: "<<
-          "Gmapping or Navigation\n"<<
-          "////////////////////////////////////////////The defualt is Navigation"<<endl;
+    else{
+        ROS_INFO("Enable one of the modes:");
+        ROS_INFO("                        Gmapping or Navigation");
+        ROS_INFO("                        The defualt is Navigation");
+    }
     if(success)
     {
-      result_.sequence = feedback_.sequence;
+      result_.result = NavfileResults;
       ROS_INFO("%s: Succeeded", action_name_.c_str());
       // set the action state to succeeded
-      as_.setSucceeded(result_);
+      asrtmode_.setSucceeded(result_);
+    }
+    else {
+      result_.result = "Robot has a problem";
+      ROS_INFO("%s: Aborted from rtmode server", action_name_.c_str());
+      asrtmode_.setAborted(result_);
     }
   }
-
 
 };
 
@@ -152,6 +243,7 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "rtmode_server");
 
   RobotmodeAction Robotmode("Robotmode");
+
   ros::spin();
 
   return 0;
